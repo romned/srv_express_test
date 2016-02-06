a68c414903201e3a05f9f30bf7a9c084d62d61be
romned <rom.ned@gmail.com>
2016-02-06 22:39:59 +0300

    First_example

--- main.js
+++ main.js
@@ -3,30 +3,31 @@ var express = require('express'),
     app = express(),
     server;
 
-var store= {
+var store = {
     home: {
         page: 'Практические уроки JS',
-        content: 'Home, select home'
-    },
-    about: {
-        page: 'Tutorial 1',
-        content: 'Хороший день!'
-    },
-    day2: {
-        page: 'Tutorial 2',
-        content: 'Новые пробы'
-    },
-    daynext: {
-        page: 'Tutorial 3',
-        content: 'Продолжение работ'
+        content: 'example:'
     }
+    /*   about: {
+     page: 'Tutorial 1',
+     content: 'Хороший день!'
+     },
+     day2: {
+     page: 'Tutorial 2',
+     content: 'Новые пробы'
+     },
+     daynext: {
+     page: 'Tutorial 3',
+     content: 'Продолжение работ'
+     }
+     */
 }
 
 storeKeys = Object.keys(store);
 
 app.set('view engine', 'jade');
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     console.log('%s %s', req.method, req.url);
     next();
 });
@@ -34,15 +35,15 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extendet: true}));
 
 app.route('/new')
-    .get(function(req, res){
-    res.render('new', {
-        page: 'Add New',
-        links: storeKeys
-    });
-})
-    .post(function(req, res){
+    .get(function (req, res) {
+        res.render('new', {
+            page: 'Add New',
+            links: storeKeys
+        });
+    })
+    .post(function (req, res) {
         var data = req.body;
-        if(data.pageurl && data.pagename && data.pagecontent) {
+        if (data.pageurl && data.pagename && data.pagecontent) {
             store[data.pageurl] = {
                 page: data.pagename,
                 content: data.pagecontent
@@ -52,13 +53,13 @@ app.route('/new')
         res.redirect('/');
     });
 
-app.get('/about', function(req, res){
+app.get('/about', function (req, res) {
     res.render('about', {
         links: storeKeys
     });
 });
 
-app.get('/:page?', function(req, res){
+app.get('/:page?', function (req, res) {
     var page = req.params.page, data;
     if (!page) page = 'home';
     data = store[page];
@@ -67,6 +68,6 @@ app.get('/:page?', function(req, res){
     res.render("main", data);
 });
 
-server = app.listen(3000, function(){
+server = app.listen(3000, function () {
     console.log("Listening on port 3000");
 });
